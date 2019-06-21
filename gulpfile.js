6b24a9423f1d19295b5fe693cb9ecd365fdcbc11
Federico <fdsosa.35@gmail.com>
2019-06-21 17:14:29 -0300

    Changing the css files to scss

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var gulp        = require('gulp');
+var clean       = require('gulp-clean');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var teddy       = require('gulp-teddy').settings({
+    setTemplateRoot: 'src/templates/'
+});
+var sass          = require('gulp-sass');
+    sass.compiler = require('node-sass');
+
+gulp.task('clean', function() {
+    console.log('Clean temporal files has started');
+    
+    return gulp
+        .src('./.temp', { allowEmpty: true })
+        .pipe(clean());
+});
+
+gulp.task('build:html', function () {
+    console.log('Build HTML has started');
+
+    return gulp
+      .src([
+        './src/*.html',
+        '!.src/templates'
+      ])
+      .pipe(teddy.compile())
+      .pipe(gulp.dest('./.temp'))
+      .pipe(reload({ stream: true }));
+});
+
+gulp.task('build:html:prod', function () {
+  console.log('Build HTML has started');
+
+  return gulp
+    .src([
+      './src/*.html',
+      '!.src/templates'
+    ])
+    .pipe(teddy.compile())
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('build:sass', function () {
+    console.log('Build sass files has started');
+
+    return gulp
+        .src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./.temp/css'));
+});
+
+gulp.task('build:sass:prod', function () {
+  console.log('Build sass files has started');
+
+  return gulp
+      .src('./src/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build:files', function () {
+    console.log('Build files has started');
+  
+    return gulp
+      .src([
+        './src/*',
+        './src/**/*.jpg',
+        '!./src/templates',
+        '!./src/scss',
+        '!./src/*.html',
+        '!./src/**/*.scss'
+      ])
+      .pipe(gulp.dest('./.temp'))
+      .pipe(reload({ stream: true }));
+});
+
+gulp.task('build:files:prod', function () {
+  console.log('Build files has started');
+
+  return gulp
+    .src([
+      './src/*',
+      '!./src/templates',
+      '!./src/*.html',
+      '!./src/**/*.scss'
+    ])
+    .pipe(gulp.dest('./dist'))
+});
+  
+gulp.task('build:dev', gulp.series(['clean', 'build:html', 'build:sass', 'build:files']));
+
+gulp.task('build:prod', gulp.series('build:html:prod', 'build:sass:prod', 'build:files:prod'));
+
+gulp.task('watch', gulp.series(['build:dev'], function(done) {
+    gulp.watch('./src/**/*', gulp.series(['build:dev']));
+    done();
+}));
+
+gulp.task('serve', gulp.series(['watch'], function () {
+    console.log("Serve has started");
+    
+    browserSync({
+      server: {
+        baseDir: './.temp'
+      }
+    });
+}));
