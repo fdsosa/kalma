0e5813ceaf1e4f28b8f4fa3b8935d3b4ed5b2e03
Federico <fdsosa.35@gmail.com>
2019-06-30 20:47:34 -0300

    Settings for deploy in Heroku

--- server.js
+++ server.js
@@ -8,19 +8,14 @@ var folderBuild = env == 'PROD' ? '/dist/' : '/.temp/';
 server.use(express.static(path.join(__dirname, folderBuild)));
 
 server.get('/', function (req, res) {
-  // console.log('Esta es la raiz de mi sitio');
-  // res.status(200).send('Bienvenido a mi servidor');
   return res.sendFile(path.join(__dirname, folderBuild, 'index.html'));
 });
 
 server.get('/home', function (req, res) {
-  // console.log('Esta es la raiz de mi sitio');
-  // res.status(200).send('Bienvenido a mi servidor');
   return res.sendFile(path.join(__dirname, folderBuild, 'index.html'));
 });
 
 server.get('/contact', function (req, res) {
-  // res.status(200).send('Pagina de Contacto');
   return res.sendFile(path.join(__dirname, folderBuild, 'contact.html'));
 });
 
